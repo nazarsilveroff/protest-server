ff005bb0ed750f159e1c1ee778b8ed6ba627004b
Vlad Krizhanivskyi <vlad.krizhanivskyi@gmail.com>
2022-04-25 19:34:56 +0300

    auth

--- src/middlewares/authorize.middleware.js
+++ src/middlewares/authorize.middleware.js
@@ -1,6 +1,6 @@
 const {Unauthorized, Forbidden} = require("http-errors");
 const jsonwebtoken = require("jsonwebtoken");
-const {getConfig} = require("../../../config");
+const {getConfig} = require("../config");
 
 
 
